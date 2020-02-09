c7662be1929665dcf406d6bf1ecefd4a48b4ef1c
kelvin <chengwj@bc.edu>
2020-02-09 00:12:47 -0500

    Completed - Ch. 5 3 Working with Multiple View Controllers + Segues

---
+++ ToDo List/ToDoDetailTableViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ToDoDetailTableViewController.swift
+//  ToDo List
+//
+//  Created by Kelvin Cheng on 2/9/20.
+//  Copyright © 2020 Kelvin Cheng. All rights reserved.
+//
+
+import UIKit
+
+class ToDoDetailTableViewController: UITableViewController {
+    
+    @IBOutlet weak var saveBarButton: UIBarButtonItem!
+    @IBOutlet weak var nameField: UITextField!
+    @IBOutlet weak var datePicker: UIDatePicker!
+    @IBOutlet weak var noteView: UITextView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    @IBAction func cancelButtonPressed(_ sender: UIBarButtonItem) {
+        let isPresentingInAddMode = presentingViewController is UINavigationController
+        if isPresentingInAddMode {
+            dismiss(animated: true, completion: nil)
+        }
+        else {
+            navigationController?.popViewController(animated: true)
+        }
+    }
+}
