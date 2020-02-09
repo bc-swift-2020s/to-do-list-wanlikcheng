425441c063b84832c7f077ea4c3ccd40be98c5aa
kelvin <chengwj@bc.edu>
2020-02-09 18:34:02 -0500

    Completed - 5 6 Structs

---
+++ ToDo List/ToDoItem.swift
@@ -0,0 +1,15 @@
+//
+//  ToDoItem.swift
+//  ToDo List
+//
+//  Created by Kelvin Cheng on 2/9/20.
+//  Copyright © 2020 Kelvin Cheng. All rights reserved.
+//
+
+import Foundation
+
+struct ToDoItem {
+    var name: String
+    var date: Date
+    var notes: String
+}
